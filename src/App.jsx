ac3f78df544a28eca7ae6cd0eb805f62c65ab0f8
giokhvede <148794939+giokhvede@users.noreply.github.com>
2024-02-22 04:53:21 +0400

    added active styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,17 @@ import equilibrium from "/images/image-equilibrium.jpg";
 import ethereum from "/images/icon-ethereum.svg";
 import clock from "/images/icon-clock.svg";
 import avatar from "/images/image-avatar.png";
+import view from "/images/icon-view.svg";
 
 function App() {
   return (
     <div className="container">
-      <img src={equilibrium} className="equilibrium" alt="equilibrium" />
+      <div className="equilibrium-container">
+        <div className="view-container">
+          <img src={view} alt="view" className="view" />
+        </div>
+        <img src={equilibrium} className="equilibrium" alt="equilibrium" />
+      </div>
       <h1>Equilibrium #3429</h1>
       <p className="equilibrium-text">
         Our Equilibrium collection promotes balance and calm.
